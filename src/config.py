b006203349a7191caa7c1e0df20078c35d818b83
Victor Roman <rromanss23@gmail.com>
2025-09-20 17:34:52 +0200

    Update config

--- src/config.py
+++ src/config.py
@@ -8,24 +8,9 @@ load_dotenv()
 
 # Load API keys
 youtube_api_key = os.getenv('YOUTUBE_API_KEY')
-if not youtube_api_key:
-    logger.error("❌ Please set YOUTUBE_API_KEY in .env file")
-    exit(1)
 gemini_api_key = os.getenv('GEMINI_API_KEY')
-if not gemini_api_key:
-    logger.error("❌ Please set GEMINI_API_KEY in .env file")
-    exit(1)
-    
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 chat_id = os.getenv('TELEGRAM_CHAT_ID')
-
-if not bot_token:
-    print("Set TELEGRAM_BOT_TOKEN in .env file")
-    exit(1)
-    
-if not chat_id:
-    print("Set TELEGRAM_CHAT_ID in .env file")
-    exit(1)
 
 # Channels to monitor
 youtube_channels = ["@JoseLuisCavatv", "@nacho_ic"]
