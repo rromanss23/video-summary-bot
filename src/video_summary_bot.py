a46d0b482713c07248a83d2d4226cd3371b87d1f
Victor Roman <rromanss23@gmail.com>
2025-09-22 17:15:41 +0200

    update

--- src/video_summary_bot.py
+++ src/video_summary_bot.py
@@ -13,6 +13,9 @@ yt = YouTubeHandler(youtube_api_key)
 gemini = GeminiHandler(gemini_api_key)
 telegram = TelegramHandler(bot_token, chat_id)
 
+# TODO: schedule the script to run daily on weekdays
+# If time is 10.30AM on a weekday, Madrid time, run for channel @JoseLuisCavatv
+# if time is 12.30PM on a weekday, Madrid time, run for channel @nacho_ic
 for channel in youtube_channels:
     logger.info(f"Checking channel: {channel}")
     
