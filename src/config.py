2c094fc2006be26e83e7ae4099d73e91c28e8570
Victor Roman <rromanss23@gmail.com>
2025-10-02 12:04:52 +0200

    update users

--- src/config.py
+++ src/config.py
@@ -29,8 +29,13 @@ user_preferences = {
         'wants_news': True
     },
     '1021945174': { # Alberto Salcedo
-        'channels': ['@JoseLuisCavatv', '@nacho_ic'],
+        'channels': ['@JoseLuisCavatv', '@nacho_ic', '@juanrallo', '@bravosresearchcrypto', '@bravosresearch'],
         'user_name': 'Alberto Salcedo',
-        'wants_news': True
+        'wants_news': False
+    },
+    '8467652191': { # Javier Roman
+        'channels': ['@JoseLuisCavatv', '@nacho_ic', '@juanrallo', '@bravosresearchcrypto', '@bravosresearch'],
+        'user_name': 'Javier Roman',
+        'wants_news': False
     }
 }
