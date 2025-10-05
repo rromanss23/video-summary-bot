c77c66ddae502f0b9e009bf66d2345684bec4292
Victor Roman <rromanss23@gmail.com>
2025-10-05 12:04:52 +0200

    restore users

---
+++ src/video_summary_bot/config/users.py
@@ -0,0 +1,19 @@
+"""User preferences configuration
+
+Note: Update with actual user IDs and preferences and run migration script to move to DB
+"""
+
+user_preferences = {
+    '12345678': {  # John Smith
+        'channels': ["@TechNewsDaily", '@FinanceInsights', '@CryptoAnalysis', '@MarketWatch', '@InvestmentTips'],
+        'user_name': 'John Smith'
+    },
+    '23456789': {  # Sarah Johnson
+        'channels': ['@TechNewsDaily', '@FinanceInsights', '@CryptoAnalysis', '@MarketWatch', '@InvestmentTips'],
+        'user_name': 'Sarah Johnson'
+    },
+    '34567890': {  # Michael Brown
+        'channels': ['@TechNewsDaily', '@FinanceInsights', '@CryptoAnalysis', '@MarketWatch', '@InvestmentTips'],
+        'user_name': 'Michael Brown'
+    }
+}
